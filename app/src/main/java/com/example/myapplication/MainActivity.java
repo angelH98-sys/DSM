5b080fdda26ab33d3ad269840a13afc23ea3af18
angel <angelH98@outlook.com>
2021-02-04 16:28:06 -0600

    Clase 2 teo

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText1, editText2;
+    private TextView textView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText1 = (EditText) findViewById(R.id.et1);
+        editText2 = (EditText) findViewById(R.id.et2);
+        textView1 = (TextView) findViewById(R.id.tv1);
+    }
+
+    public void sumar(View view){
+
+        int num1 = Integer.parseInt(editText1.getText().toString());
+        int num2 = Integer.parseInt(editText2.getText().toString());
+
+        int suma = num1 + num2;
+
+        String result = String.valueOf(suma);
+
+        textView1.setText(result);
+
     }
 }
